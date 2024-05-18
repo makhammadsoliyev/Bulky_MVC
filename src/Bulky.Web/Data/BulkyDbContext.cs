975192609551f9ebbae23bf49731823f0133a6c7
Umidjon <makhammadsoliyev@gmail.com>
2024-05-18 14:35:51 +0500

    Add Migrations

--- src/Bulky.Web/Data/BulkyDbContext.cs
+++ src/Bulky.Web/Data/BulkyDbContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bulky.Web.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bulky.Web.Data;
 
 public class BulkyDbContext : DbContext
 {
     public BulkyDbContext(DbContextOptions<BulkyDbContext> options) : base(options) { }
+
+    public DbSet<Category> Categories { get; set; }
 }
--- src/Bulky.Web/Models/Category.cs
+++ src/Bulky.Web/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace Bulky.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Web.Models;
 
 public class Category
 {
+    [Key]
     public long Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public int DisplayOrder { get; set; }
 }
